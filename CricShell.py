07b0140c084f6e6b363214f6313b34eb3ca2e771
Sripathy Ramaswamy <sripathyfication@gmail.com>
2017-08-21 22:48:48 -0700

    pretty printing with PrettyTable

--- CricShell.py
+++ CricShell.py
@@ -1,3 +1,4 @@
+from prettytable import PrettyTable
 import sys, cmd, os
 from cricli import *
 import datetime
@@ -12,17 +13,16 @@ class CricShell(cmd.Cmd):
     def do_list(self,arg):
         ''' Fetch the current ongoing international matches '''
         _matches = self.cric_inst.get_ongoing_matches()
+        _header = "\t\t\t\tInternational Matches"
 
-        _head_string = "Match Id\t Date \t\t Team-1\t\t\t\tTeam-2"
-        print "\t\t International Matches \n"
-        print _head_string
-        print "-  "*len(_head_string)
-
+        print "\n"+_header+"\n"
+        table = PrettyTable(["Match-Id","Date","Team-1","Team-2"])
         for match in _matches:
             date = datetime.datetime.strptime(match['date'],"%Y-%m-%dT%H:%M:%S.%fZ")
             date_new = date.strftime("%d-%m-%Y")
-            print str(match['unique_id'])+" \t"+str(date_new)+"  "+match['team-1']+"\t\t\t\t"+match['team-2']
-
+            table.add_row([match['unique_id'],date_new,match['team-1'],match['team-2']])
+        print table
+        
     def do_watch(self,arg):
         ''' Watch for updates on a match continuously unless interrupted
             Usage: watch <match-id>
@@ -40,14 +40,13 @@ class CricShell(cmd.Cmd):
 
     def do_help(self, arg):
         print "\n\t\t ==== CricShell Help Menu ==== "
-        print "\tlist    --- List all ongoing international cricket matches"
+        print "\tlist  --- List all ongoing international cricket matches"
         print "\tscore --- Fetch the score for match <match-id>"
         print "\twatch --- Watch for updates to match <match-id>"
         print "\tstop  --- Stop watching for updates to match <match-id>"
         print "\tclear --- Clear screen."
         print "\texit  --- Exit cricShell. Are you sure you want to go to espncricinfo? "
 
-        print "\t\t**    End Help   ** "
     def do_clear(self,arg):
         os.system('clear')
 
